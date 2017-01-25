27f5bbf9216e6538dff4ffc014a40fb070bc7017
Guenter Schafranek <gschafra@web.de>
2017-01-25 17:04:00 +0100

    Starting implementing todo creation

--- client/src/app/components/todos/todos.component.ts
+++ client/src/app/components/todos/todos.component.ts
@@ -20,4 +20,19 @@ export class TodosComponent implements OnInit {
     })
   }
 
+  addTodo(event, todoText){
+    var result;
+    var newTodo = {
+      title: todoText.value,
+      isCompleted: false
+    };
+
+    result = this._todoService.saveTodo(newTodo);
+    result.subscribe(x => {
+      this.todos.push(newTodo);
+      todoText.value = '';
+    })
+
+  }
+
 }
